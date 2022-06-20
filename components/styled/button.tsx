11b9ca858533a9fbd15dcd806cbac69bc5b97ffa
JesusJMM <jesusjaviermarcanomora.2005@gmail.com>
2022-06-19 23:28:34 -0400

    style changes

--- components/styled/button.tsx
+++ components/styled/button.tsx
@@ -3,7 +3,7 @@ import {styled } from '../../stitches.config'
 export const Button = styled('button', {
   padding: '6px 12px',
   borderRadius: '8px',
-  fontSize: '15px',
+  // fontSize: '14px',
   fontWeight: 'bold',
   border: '1px solid transparent',
   backgroundColor: '$gray5',
