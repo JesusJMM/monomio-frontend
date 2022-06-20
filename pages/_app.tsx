f52cee19ef2f35e872a831fd4feb833e365f4e56
JesusJMM <jesusjaviermarcanomora.2005@gmail.com>
2022-06-19 21:26:58 -0400

    change body background color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalCss } from '../stitches.config'
+
+const globalStyles = globalCss({
+  'body': {
+    backgroundColor: '$gray1'
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles()
   return <Component {...pageProps} />
 }
 
